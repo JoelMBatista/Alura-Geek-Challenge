bc6739625fa7f8251bf57364a7e6094e1411c764
JoelMBatista <joelmatosb@outlook.com>
2023-07-19 17:12:29 -0400

    por fin

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -2,6 +2,6 @@ const btnLogin = document.getElementById('btn_login');
 
 btnLogin.addEventListener('click', () => {
 
-  window.location.replace("../screen/login.html");
+  window.location.replace(".../screen/login.html");
 
 })
