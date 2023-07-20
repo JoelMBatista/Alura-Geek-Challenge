59e1190fa5c49b8caa1d9c6344912bfcbcf94e6a
JoelMBatista <joelmatosb@outlook.com>
2023-07-20 16:33:29 -0400

    changes

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -2,6 +2,7 @@ const btnLogin = document.getElementById('btn_login');
 
 btnLogin.addEventListener('click', () => {
 
-  window.location.href = "../screen/login.html";
+  // window.location.href = "../screen/login.html";
+  window.location.replace("../Alura-ecommerce/screen/login.html");
 
 })
